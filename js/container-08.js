065456edb89a18518b6e7d44cfa70fbdaee3391c
furium <shouldBnewB@gmail.com>
2023-11-22 19:04:37 +0900

    (add) animation

---
+++ js/container-08.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+  let _name = "WEBS";
+  let userNameInput = document.getElementsByName("userName")[0];
+  let userName = userNameInput.values;
+  document.getElementById("userNameRes").innerHTML = "WEBS";
+};
